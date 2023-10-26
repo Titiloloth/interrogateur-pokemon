f5c10628f2657b9fd41e5e8d8c70727a83d05226
lmorillo <laura.mmails@gmail.com>
2023-10-26 16:18:57 +0200

    help

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
-// Méthode Fetch
+// Mon apparence
+
 document.getElementById("modal").style.display = 'none'
+let radioName = document.getElementById("radioName")
+let radioElement = document.getElementById("radioElement")
+
+
+// Mes API
 
 let datasFetch;
 const urlApi = "https://pokebuildapi.fr/api/v1/pokemon/limit/25";
@@ -9,8 +15,20 @@ async function getDataFetch () {
     datasFetch = await res.json();
 }
 
-// Liste de pokemons 
+let datasFetchTypes;
+const urlApiTypes = "https://pokebuildapi.fr/api/v1/types";
+await getDataFetchTypes();
+async function getDataFetchTypes () {
+    const resTypes = await fetch(urlApiTypes);
+    datasFetchTypes = await resTypes.json();
+}
 
+// NOMS
+
+//Affichage liste 
+
+radioName.addEventListener("click", function(){
+document.querySelector("select").options.length = 0
 datasFetch.forEach(unPokemon => {
     let option = document.createElement('option')
     option.textContent = unPokemon.name;
@@ -18,12 +36,14 @@ datasFetch.forEach(unPokemon => {
     document.querySelector("select").appendChild(option);
 })
 
+//Appui bouton
+
 document.querySelector("button").addEventListener("click", function(){
     let choix = document.querySelector("select").value;
-    AfficherPokemon(choix);
+    AfficherPokemonParNom(choix);
 })
 
-function AfficherPokemon(choix) {
+function AfficherPokemonParNom(choix) {
     let pokemonActif = datasFetch.find( pokemon => pokemon.name == choix);
     document.getElementById("modal").style.display = 'flex';
     let nomPokemon = document.getElementById("nompokemon");
@@ -39,9 +59,36 @@ function AfficherPokemon(choix) {
     let divTypesPokemon = document.getElementById("typespokemon")
     divTypesPokemon.innerText = typesPokemon
 }
+})
 
+// ELEMENTS
 
+//Affichage liste 
 
+radioElement.addEventListener("click", function(){
+    document.querySelector("select").options.length = 0
+    datasFetchTypes.forEach(unType => {
+        let optionTypes = document.createElement('option')
+        optionTypes.textContent = unType.name;
+        optionTypes.value = unType.name;
+        document.querySelector("select").appendChild(optionTypes);
+    })
+    })
 
+    //Appui bouton
+
+    document.querySelector("button").addEventListener("click", function(){
+        let choix = document.querySelector("select").value;
+        AfficherPokemonParElement(choix);
+    })
+
+    function AfficherPokemonParElement(choix) {
+        let elementActif = datasFetch.find( elementPokemon => elementPokemon.apiTypes == choix);
+        document.getElementById("modal").style.display = 'flex';
+        // let imagePokemon2 = document.getElementById("imagepokemon");
+        // imagePokemon2.style.display = 'none'
+        let nomPokemon = document.getElementById("nompokemon");
+        nomPokemon.textContent = "Voici les pokemons de ce type : " + elementActif
+    }
 
 
